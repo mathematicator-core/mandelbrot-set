94cbf75b07c4d37d5b9cb00fa04456e8ced440d4
Jan Barášek <janbarasek@gmail.com>
2020-05-22 11:56:02 +0200

    Translate all comments to EN and fix grammer

--- src/MandelbrotSet.php
+++ src/MandelbrotSet.php
@@ -9,7 +9,7 @@ use Nette\Utils\FileSystem;
 
 /**
  * Calculate and render Mandelbrot set as image to file.
- * Implementation is inspired by Pavol Hejny, https://www.pavolhejny.com/.
+ * Implementation was inspired from Pavol Hejny, https://www.pavolhejny.com/.
  */
 final class MandelbrotSet
 {
@@ -64,15 +64,15 @@ final class MandelbrotSet
 		$alpha_color = imagecolorallocatealpha($im, 0, 0, 0, 127);
 		imagefill($im, 0, 0, $alpha_color);
 
-		for ($y = 0; $y <= $dim_y; $y++) { // Procházení a vyhodnocení každého bodu
-			for ($x = 0; $x <= $dim_x; $x++) { // Zjištění souřadnic bodu, který se přičte v každé iteraci
+		for ($y = 0; $y <= $dim_y; $y++) { // browsing and evaluating each point
+			for ($x = 0; $x <= $dim_x; $x++) { // find the coordinates of the point that is added in each iteration
 				$c1 = $min_x + ($max_x - $min_x) / $dim_x * $x;
 				$c2 = $min_y + ($max_y - $min_y) / $dim_y * $y;
-				$z1 = 0; // aktuální číslo
+				$z1 = 0; // current number
 				$z2 = 0;
 
-				for ($i = 0; $i < $itt; $i++) { // Main iterator
-					// Zjištění vzdálenosti od 0+0i
+				for ($i = 0; $i < $itt; $i++) { // main iterator
+					// finding the distance from 0 + 0i
 					$distance = sqrt($z1 * $z1 + $z2 * $z2);
 
 					if ((int) $distance !== 0) {
@@ -81,26 +81,26 @@ final class MandelbrotSet
 						$angle = 0;
 					}
 
-					if ($z2 < 0) { // Úhel
+					if ($z2 < 0) { // angle
 						$angle = (2 * M_PI) - $angle;
 					}
 
-					$angle *= $d1; // Vynásobení úhlu
-					$distance = $distance ** $d2; // Mocnění vzdálenosti
-					// Výpočet nového x,y
+					$angle *= $d1; // multiply the angle
+					$distance = $distance ** $d2; // power of distance
+					// calculation of the new x, y
 					$z1 = cos($angle) * $distance;
 					$z2 = sin($angle) * $distance;
-					// Přičtení souřadnic bodu
+					// adding point coordinates
 					$z1 += $c1;
 					$z2 += $c2;
 
-					// Pokud je bod ve vzdálenosti 2 nebo větší, bod v množině nebude a iterování lze ukončit
+					// if the point is at a distance of 2 or greater, the point will not be in the set and the iteration can be terminated
 					if ($z1 * $z1 + $z2 * $z2 >= 4) {
 						break;
 					}
 				}
 
-				// Pokud v každé iteraci držel nový bod ve vzdálenosti 2 nebo méně, je původní bod vyplněn.
+				// if in each iteration he held a new point at a distance of 2 or less, the original point is filled.
 				if ($i >= $itt) {
 					imagesetpixel($im, (int) round($x), (int) round($y), $blackColor);
 				}
