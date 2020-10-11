18b125fa593c9984b84758b8a4840c05386ddea2
Jan Barášek <janbarasek@gmail.com>
2020-10-11 20:02:03 +0200

    Extension: Add support for custom parameters + robust logic for get tempDir.

--- src/MandelbrotSetExtension.php
+++ src/MandelbrotSetExtension.php
@@ -6,16 +6,43 @@ namespace Mathematicator\MandelbrotSet;
 
 
 use Nette\DI\CompilerExtension;
+use Nette\DI\ContainerBuilder;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 
 final class MandelbrotSetExtension extends CompilerExtension
 {
+	public function getConfigSchema(): Schema
+	{
+		return Expect::structure([
+			'tempDir' => Expect::string(),
+		])->castTo('array');
+	}
+
+
 	public function beforeCompile(): void
 	{
 		$builder = $this->getContainerBuilder();
 
 		$builder->addDefinition($this->prefix('mandelbrotSet'))
 			->setFactory(MandelbrotSet::class, [
-				'tempDir' => '%tempDir%/mandelbrot-set', // TODO: Check it
+				'tempDir' => $this->processTempDir($builder),
 			]);
+	}
+
+
+	private function processTempDir(ContainerBuilder $builder): string
+	{
+		if (isset($this->config['tempDir']) === true) {
+			return $this->config['tempDir'];
+		}
+		if (isset($builder->parameters['tempDir']) === false) {
+			throw new \RuntimeException('Configuration parameter "tempDir" is not defined. Please define custom temp dir.');
+		}
+		if (\is_dir($builder->parameters['tempDir']) === false) {
+			throw new \RuntimeException('Temp path "' . $builder->parameters['tempDir'] . '" is not directory.');
+		}
+
+		return $builder->parameters['tempDir'] . '/mandelbrot-set';
 	}
 }
